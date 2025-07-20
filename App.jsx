85f7a7366240e4e5d9a56d0b4565583443eb7f47
Mushood <khawaja.muhammad.mushood@gmail.com>
2025-07-20 14:38:36 +0500

    feat: Add profile management screens and update gitignore for lock files

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { PublicStack } from './src/navigations';
+import { StatusBar } from 'react-native';
 
 function App() {
 
   return (
     <GestureHandlerRootView>
+      <StatusBar hidden={true} />
       <NavigationContainer>
         <PublicStack />
       </NavigationContainer>
